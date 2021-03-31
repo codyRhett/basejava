2b9cd20c43196a2e3512daea07e54aa72fae59a0
artem <artemryvkin@yandex.ru>
2021-03-31 22:55:50 +0300

    сделал вызов метода toString через Reflection в MainReflection.java

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -1,21 +1,14 @@
 import ru.javawebinar.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
-//        Resume r = new Resume();
-//        Field field = r.getClass().getDeclaredFields()[0];
-//        field.setAccessible(true);
-//        System.out.println(field.getName());
-//        System.out.println(field.get(r));
-//        field.set(r, "new_uuid");
-//        //TODO resume.toString вызвать через отражение
-//        System.out.println(r);
-
-        Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
-
-        System.out.println(r);
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume("uuid");
+        Method method = r.getClass().getDeclaredMethod("toString");
+        String str = (String) method.invoke(r);
+        System.out.println(str);
     }
 }
