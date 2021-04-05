c394ad963a1a5b65807275e2a3448d0c12ea0599
katkov <katkov@mpzvpk.ru>
2021-04-05 14:13:02 +0300

    начал делать коллекции

---
+++ src/ru/javawebinar/storage/AbstractStorage.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.storage;
+
+import ru.javawebinar.exception.ExistStorageException;
+import ru.javawebinar.exception.NotExistStorageException;
+import ru.javawebinar.exception.StorageException;
+import ru.javawebinar.model.Resume;
+
+import java.util.Arrays;
+
+public abstract class AbstractStorage implements Storage {
+
+
+}
