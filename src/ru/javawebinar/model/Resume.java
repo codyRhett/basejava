3260fdbac7a69addd2e99bf132e3bf2a2550cead
codyRhett <artemryvkin@yandex.ru>
2021-04-18 23:23:26 +0300

    сделал comparator для resume

--- src/ru/javawebinar/model/Resume.java
+++ src/ru/javawebinar/model/Resume.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 /**
  * Initial resume class
  */
-public class Resume implements Comparable<Resume>{
+public class Resume{
     // Unique identifier
     private String uuid;
 
@@ -41,8 +41,8 @@ public class Resume implements Comparable<Resume>{
         return uuid;
     }
 
-    @Override
-    public int compareTo(Resume o) {
-        return uuid.compareTo(o.uuid);
-    }
+//    @Override
+//    public int compareTo(Resume o) {
+//        return uuid.compareTo(o.uuid);
+//    }
 }
