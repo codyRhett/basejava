4caf9c56557ecbc3095b1b6ee43e0af8eb13a0eb
katkov <katkov@mpzvpk.ru>
2021-05-12 17:11:37 +0300

    практически написал ResumeTestData

--- src/ResumeTestData.java
+++ src/ResumeTestData.java
@@ -23,30 +23,59 @@ public class ResumeTestData {
         storage.save(new Resume(UUID_2, NAME_2));
 
         Section position = new SectionText(SectionType.POSITION.getTitle());
-        ((SectionText) position).setText("программист JAVA");
+        ((SectionText) position).setText("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
 
         Section personal = new SectionText(SectionType.PERSONAL.getTitle());
-        ((SectionText) personal).setText("целеустремленный, обучаемый");
+        ((SectionText) personal).setText("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры.");
 
         Section achievements = new SectionList(SectionType.ACHIEVEMENT.getTitle());
-        ((SectionList) achievements).addTextToList("Достижение 1");
-        ((SectionList) achievements).addTextToList("Достижение 2");
-        ((SectionList) achievements).addTextToList("Достижение 3");
+        ((SectionList) achievements).addTextToList("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", " +
+                "\"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)" +
+                "\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.");
+        ((SectionList) achievements).addTextToList("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. " +
+                "Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.");
+        ((SectionList) achievements).addTextToList("Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, " +
+                "Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: " +
+                "Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.");
 
         Section qualifications = new SectionList(SectionType.QUALIFICATION.getTitle());
-        ((SectionList) qualifications).addTextToList("Квалификация 1");
-        ((SectionList) qualifications).addTextToList("Квалификация 2");
-        ((SectionList) qualifications).addTextToList("Квалификация 3");
+        ((SectionList) qualifications).addTextToList("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
+        ((SectionList) qualifications).addTextToList("Version control: Subversion, Git, Mercury, ClearCase, Perforce");
+        ((SectionList) qualifications).addTextToList("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle");
 
         Section experience = new SectionExperience(SectionType.EXPERIENCE.getTitle());
-        Organization siemensOrg = new Organization("Siemens");
-        Experience experienceSiemens = new Experience();
-//        //experienceSiemens.setDate();
-        experienceSiemens.setPosition("Инженер");
-        experienceSiemens.setResponsibilities("Разработка систем управления");
-        siemensOrg.addExperienceToList(experienceSiemens);
+        Organization orgJavaOnline = new Organization("Java Online Projects");
+        Experience experienceJavaOrg = new Experience();
+        experienceJavaOrg.setPosition("Автор проекта");
+        experienceJavaOrg.setResponsibilities("Создание, организация и проведение Java онлайн проектов и стажировок.");
+        orgJavaOnline.addExperienceToList(experienceJavaOrg);
+        ((SectionExperience) experience).addOrganizationToList(orgJavaOnline);
 
-        ((SectionExperience) experience).addOrganizationToList(siemensOrg);
+        Organization orgWrike = new Organization("Wrike");
+        Experience experienceWrikeOrg = new Experience();
+        experienceWrikeOrg.setPosition("Старший разработчик (backend)");
+        experienceWrikeOrg.setResponsibilities("\tСтарший разработчик (backend)\n" +
+                "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, " +
+                "Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.");
+        orgWrike.addExperienceToList(experienceWrikeOrg);
+        ((SectionExperience) experience).addOrganizationToList(orgWrike);
+        
+        Section education = new SectionExperience(SectionType.EDUCATION.getTitle());
+        Organization CourseraOrg = new Organization("Coursera");
+        Experience educationCoursera = new Experience();
+        educationCoursera.setPosition("Магистр");
+        educationCoursera.setResponsibilities("\"Functional Programming Principles in Scala\" by Martin Odersky");
+        CourseraOrg.addExperienceToList(educationCoursera);
+
+        ((SectionExperience) education).addOrganizationToList(CourseraOrg);
+
+        Organization LuxoftOrg = new Organization("Luxoft");
+        Experience educationLuxoft = new Experience();
+        educationLuxoft.setPosition("Магистр");
+        educationLuxoft.setResponsibilities("Курс Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.");
+        LuxoftOrg.addExperienceToList(educationLuxoft);
+
+        ((SectionExperience) education).addOrganizationToList(LuxoftOrg);
 
         Resume resume = storage.get(UUID_1);
         resume.addSection(SectionType.POSITION, position);
@@ -54,10 +83,15 @@ public class ResumeTestData {
         resume.addSection(SectionType.ACHIEVEMENT, achievements);
         resume.addSection(SectionType.QUALIFICATION, qualifications);
         resume.addSection(SectionType.EXPERIENCE, experience);
+        resume.addSection(SectionType.EDUCATION, education);
 
         resume.addContact(ContactsType.MOBILENUMBER, "91234567890");
         resume.addContact(ContactsType.SKYPE, "codyrhett");
         resume.addContact(ContactsType.EMAIL, "codyrhett@yandex.ru");
+
+        for (SectionType type : SectionType.values()) {
+            System.out.prighfntln(type.getTitle());
+        }
 
         storage.save(new Resume(UUID_3, NAME_3));
     }
