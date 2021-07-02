f5e24692948d756bda0a074f74b5b80987a42f5e
katkov <katkov@mpzvpk.ru>
2021-07-02 16:49:01 +0300

    сделал примеры из уроков по Lazysingleton

---
+++ src/ru/javawebinar/util/LazySingleton.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.util;
+
+public class LazySingleton {
+    //private static final LazySingleton INSTANCE = new LazySingleton();
+    int i;
+    volatile private static LazySingleton INSTANCE;
+
+    double sin = Math.sin(13.);
+
+    private LazySingleton() {
+
+    }
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+//        if(INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if(INSTANCE == null) {
+//                    int i = 13;
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+    }
+}
