5281b0ab8bcc2614e93d738cd80cb037907b1257
katkov <katkov@mpzvpk.ru>
2021-07-05 17:12:56 +0300

    сделал пример с latch

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -3,6 +3,8 @@ import ru.javawebinar.util.LazySingleton;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MainConcurrency {
     private static final int THREADS_NUMBER = 10_000;
@@ -10,79 +12,82 @@ public class MainConcurrency {
     private static final Object LOCK1 = new Object();
     private static final Object LOCK2 = new Object();
 
-    public static void main(String[] args) {
-        new Thread(() -> {
-            func(LOCK1, LOCK2);
-        }).start();
-
-        new Thread(() -> func(LOCK2, LOCK1)).start();
-     }
-
-    private static void func(Object lockObj1, Object lockObj2) {
-        synchronized (lockObj1) {
-            System.out.println(lockObj1.toString() + " is locked");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Wait for unlocking" + lockObj2.toString());
-            synchronized (lockObj2) {
-                System.out.println("SUCCESS");
-            }
-        }
-    }
-
-//    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, InterruptedException {
-//        System.out.println(Thread.currentThread().getName());
-//        Thread thread0 = new Thread(){
-//           @Override
-//           public void run() {
-//               System.out.println(getName() + ", " + getState());
-//               throw new IllegalStateException();
-//           }
-//        };
-//        thread0.start();
+//    public static void main(String[] args) {
 //        new Thread(() -> {
-//            System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+//            func(LOCK1, LOCK2);
 //        }).start();
-//        System.out.println(thread0.getState());
 //
-//        final MainConcurrency mainConcurrency = new MainConcurrency();
-//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
-//        for (int i = 0; i < THREADS_NUMBER; i++){
-//            Thread thread = new Thread(() -> {
-//                for (int j = 0; j < 100; j++) {
-//                    // объект mainConcurrency в Heap. К нему есть очередь
-//                    // Механизм монитор обеспечивает синхронизацию
-//                    // Все потоки встанут в очередь к объекту
-//                    mainConcurrency.inc();
-//                }
-//            });
-//            thread.start();
-//            // Ждем пока 10000 потоков не закончат работать
-//            // Текущий поток (main) ждет пока не закончится данный поток - thread
-//            // Поток main останавливается и ждет пока thread не закончится
-//            //thread.join();
-//            threads.add(thread);
+//        new Thread(() -> func(LOCK2, LOCK1)).start();
+//     }
+//
+//    private static void func(Object lockObj1, Object lockObj2) {
+//        synchronized (lockObj1) {
+//            System.out.println(lockObj1.toString() + " is locked");
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("Wait for unlocking" + lockObj2.toString());
+//            synchronized (lockObj2) {
+//                System.out.println("SUCCESS");
+//            }
 //        }
-//        // Для каждого потока листа подконекчиваемся
+//    }
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread thread0 = new Thread(){
+           @Override
+           public void run() {
+               System.out.println(getName() + ", " + getState());
+               throw new IllegalStateException();
+           }
+        };
+        thread0.start();
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+        }).start();
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++){
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    // объект mainConcurrency в Heap. К нему есть очередь
+                    // Механизм монитор обеспечивает синхронизацию
+                    // Все потоки встанут в очередь к объекту
+                    mainConcurrency.inc();
+                }
+                latch.countDown();
+            });
+            thread.start();
+            // Ждем пока 10000 потоков не закончат работать
+            // Текущий поток (main) ждет пока не закончится данный поток - thread
+            // Поток main останавливается и ждет пока thread не закончится
+            //thread.join();
+            threads.add(thread);
+        }
+        // Для каждого потока листа подконекчиваемся
 //        threads.forEach(t-> {
 //            try {
 //                t.join();
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
+//            latch.countDown();
 //        });
-//
-//        //Thread.sleep(500);
-//
-//        System.out.println(mainConcurrency.counter);
-//        LazySingleton.getInstance();
-//    }
+
+        //Thread.sleep(500);
+        latch.await(10, TimeUnit.SECONDS);
+        System.out.println(mainConcurrency.counter);
+        LazySingleton.getInstance();
+    }
 
     // Делаем так что в этот метод заходит только один единственный поток
-    private void inc() {
+    private synchronized void inc() {
         //double a = Math.sin(13.0);
         // Синхронизация требуется только для увеличения counter
         // Поэтому synchronized переносим. В synchronized передаем объект
