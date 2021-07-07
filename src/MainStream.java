526585ad7f97ee2351b81eb77218ee99690f4945
katkov <katkov@mpzvpk.ru>
2021-07-07 17:01:07 +0300

    правки по замечаниям

--- src/MainStream.java
+++ src/MainStream.java
@@ -3,8 +3,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainStream {
+    public static void main(String[] args) {
+        //int[] values = {2,7,5,5,6,2,6,1,2,8,1};
+        //int[] values = {9,8};
+        int[] values = {1, 2, 3, 3, 2, 3};
+
+        System.out.println(minValue(values));
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 67, 8);
+
+        System.out.println(oddOrEven(list));
+    }
+
     private static int minValue(int[] values) {
         int[] valuesUnique = Arrays.stream(values).distinct().sorted().toArray();
+        //Arrays.stream(valuesUnique).reduce(val )
         int coeff = 10;
         float res = 0;
         for (int val : valuesUnique) {
@@ -16,23 +29,10 @@ public class MainStream {
 
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
+        int mod = integers.stream().mapToInt(i->i).sum() % 2;
+
         return integers.stream()
-                .filter(val -> (val % 2) != integers
-                        .stream()
-                        .mapToInt(i->i)
-                        .sum() % 2)
+                .filter(val -> (val % 2) != mod)
                 .collect(Collectors.toList());
-    }
-
-    public static void main(String[] args) {
-        //int[] values = {2,7,5,5,6,2,6,1,2,8,1};
-        //int[] values = {9,8};
-        int[] values = {1, 2, 3, 3, 2, 3};
-
-        System.out.println(minValue(values));
-
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 67, 8);
-
-        System.out.println(oddOrEven(list));
     }
 }
