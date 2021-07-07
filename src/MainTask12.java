92efbd0d5d5f07bef870617173a3a266110e4e16
katkov <katkov@mpzvpk.ru>
2021-07-07 09:47:11 +0300

    Сделал первое задание в 12 уроке

--- src/MainTask12.java
+++ src/MainTask12.java
@@ -1,26 +1,22 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class MainTask12 {
     private static int minValue(int[] values) {
         int[] valuesUnique = Arrays.stream(values).distinct().sorted().toArray();
-
-        int size = valuesUnique.length;
-        int temp = 1;
-        double res = 0;
-        for (int i = 0; i < size; i++) {
-            temp = temp * 10;
-            res += (double)valuesUnique[i] / (double)temp;
+        int coeff = 10;
+        float res = 0;
+        for (int val : valuesUnique) {
+            res += (float) val/ (float) coeff;
+            coeff *= 10;
         }
-
-        int result = Integer.valueOf((int) (res*temp));
-        return 1;
+        return (int) (res * coeff / 10);
     }
 
     public static void main(String[] args) {
-        int[] values = {2,7,5,5,6,2,6,1,2,8,1};
+        //int[] values = {2,7,5,5,6,2,6,1,2,8,1};
+        //int[] values = {9,8};
+        int[] values = {1,2,3,3,2,3};
 
-        minValue(values);
+        System.out.println(minValue(values));
     }
 }
